8ab456221aa5cb5fc100b323317f2e54c840bf26
efossas <serpicofossas@gmail.com>
2018-05-14 17:14:51 -0600

    Grab qengine and bengine file names every time when loading fiddle pages Authors: Eric Fossas

--- public/dev/js/playground.js
+++ public/dev/js/playground.js
@@ -32,52 +32,52 @@ PG.createCookieString = function(key,value,date,path) {
 	Set file name if page is loaded in 'play' mode	
 ***/
 
-if(window.location.href.indexOf('bengine') > 1) {
-	var bengineFileName;
-	var query = window.location.href.split('?').pop().split('#').shift().split('&');
-	
-	for(value of query) {
-		var parts = value.split('=');
-		if(parts[0] === 'demo') {
-			bengineFileName = 'demo/pid-bengine/1.0';
-		}
+// get or make bengine file name
+var bengineFileName;
+var query = window.location.href.split('?').pop().split('#').shift().split('&');
+
+for(value of query) {
+	var parts = value.split('=');
+	if(parts[0] === 'demo') {
+		bengineFileName = 'demo/pid-bengine/1.0';
 	}
-	
-	if(!bengineFileName) {
-		// bengine file
-		var existingBengineFile = PG.findCookieValue(document.cookie,'bengineFileName');
-		if(existingBengineFile) {
-			bengineFileName = existingBengineFile;
-		} else {
-			bengineFileName = PG.uuidv4();
-		}
-		var expiration = new Date(new Date().setFullYear(new Date().getFullYear() + 10));
-		var cookie = PG.createCookieString('bengineFileName',bengineFileName,expiration,'/');
-		document.cookie = cookie;
+}
+
+if(!bengineFileName) {
+	// bengine file
+	var existingBengineFile = PG.findCookieValue(document.cookie,'bengineFileName');
+	if(existingBengineFile) {
+		bengineFileName = existingBengineFile;
+	} else {
+		bengineFileName = PG.uuidv4();
 	}
-} else if(window.location.href.indexOf('qengine') > 1) {
-	var qengineFileName;
-	var query = window.location.href.split('?').pop().split('#').shift().split('&');
-	
-	for(value of query) {
-		var parts = value.split('=');
-		if(parts[0] === 'demo') {			
-			qengineFileName = 'demo/pid-qengine/1.0';
-		}
+	var expiration = new Date(new Date().setFullYear(new Date().getFullYear() + 10));
+	var cookie = PG.createCookieString('bengineFileName',bengineFileName,expiration,'/');
+	document.cookie = cookie;
+}
+
+// get or make qengine file name
+var qengineFileName;
+var query = window.location.href.split('?').pop().split('#').shift().split('&');
+
+for(value of query) {
+	var parts = value.split('=');
+	if(parts[0] === 'demo') {			
+		qengineFileName = 'demo/pid-qengine/1.0';
 	}
-	
-	if(!qengineFileName) {
-		// qengine file
-		var existingQengineFile = PG.findCookieValue(document.cookie,'qengineFileName');
-		if(existingQengineFile) {
-			qengineFileName = existingQengineFile;
-		} else {
-			qengineFileName = PG.uuidv4();
-		}
-		var expiration = new Date(new Date().setFullYear(new Date().getFullYear() + 10));
-		var cookie = PG.createCookieString('qengineFileName',qengineFileName,expiration,'/');
-		document.cookie = cookie;
+}
+
+if(!qengineFileName) {
+	// qengine file
+	var existingQengineFile = PG.findCookieValue(document.cookie,'qengineFileName');
+	if(existingQengineFile) {
+		qengineFileName = existingQengineFile;
+	} else {
+		qengineFileName = PG.uuidv4();
 	}
+	var expiration = new Date(new Date().setFullYear(new Date().getFullYear() + 10));
+	var cookie = PG.createCookieString('qengineFileName',qengineFileName,expiration,'/');
+	document.cookie = cookie;
 }
 
 
